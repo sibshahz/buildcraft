d406212ad0a2df640f0909f992ad8d65e3abe985
sibshahz <shahid5ssg@gmail.com>
2026-03-28 18:02:50 +0500

    favicon allowed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/favicon.ico'],
       disallow: ['/admin', '/api'],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
